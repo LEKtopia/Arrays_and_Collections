8fdbf8682b5148bbaaf2dccd144cc99e0b78e93b
LEKtopia <24890955+LEKtopia@users.noreply.github.com>
2019-01-20 21:57:26 -0600

    Initial commit - add files via upload

---
+++ LP2 Arrays and Collections Demo/LP2 Arrays and Collections Demo/Form1.Designer.vb
@@ -0,0 +1,61 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstCollections = New System.Windows.Forms.ListBox()
+        Me.btnDemo = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lstCollections
+        '
+        Me.lstCollections.FormattingEnabled = True
+        Me.lstCollections.Location = New System.Drawing.Point(12, 8)
+        Me.lstCollections.Name = "lstCollections"
+        Me.lstCollections.Size = New System.Drawing.Size(230, 225)
+        Me.lstCollections.TabIndex = 0
+        '
+        'btnDemo
+        '
+        Me.btnDemo.Location = New System.Drawing.Point(86, 239)
+        Me.btnDemo.Name = "btnDemo"
+        Me.btnDemo.Size = New System.Drawing.Size(75, 23)
+        Me.btnDemo.TabIndex = 1
+        Me.btnDemo.Text = "Go Go Demo"
+        Me.btnDemo.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(264, 293)
+        Me.Controls.Add(Me.btnDemo)
+        Me.Controls.Add(Me.lstCollections)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lstCollections As ListBox
+    Friend WithEvents btnDemo As Button
+End Class
---
+++ LP2 Arrays and Collections Demo/LP2 Arrays and Collections Demo/Form1.vb
@@ -0,0 +1,40 @@
+﻿Option Strict On
+Option Explicit On
+
+Public Class Form1
+
+    Dim anArrayOfNumbers As Integer()
+    Dim aListOfWords As New List(Of String)     ' Specify data type
+    Dim anArrayListOfStuff As New ArrayList()   ' Don't specify data type, can hold more than one
+
+    Dim aDictionary As New Dictionary(Of String, Integer) ' Two data types, contains key value pairs
+
+    Private Sub btnDemo_Click(sender As Object, e As EventArgs) Handles btnDemo.Click
+        anArrayOfNumbers = {2, 1, 5, 7}
+
+        ' List demo
+        aListOfWords.Add("First")
+        aListOfWords.Add("Second")
+        aListOfWords.Add("Third")
+        aListOfWords.Add("Fourth")
+
+        ' Array list demo
+        anArrayListOfStuff.Add("A String")
+        anArrayListOfStuff.Add(1)
+
+        ' Dictionary demo
+        aDictionary.Add("Key_1", 32)
+        aDictionary.Add("Key_2", 42)
+
+
+
+        lstCollections.Items.Add(anArrayOfNumbers(0))
+        lstCollections.Items.Add(aListOfWords(0))
+        lstCollections.Items.Add(anArrayListOfStuff(0))
+        lstCollections.Items.Add(anArrayListOfStuff(1))
+
+        ' lstCollections.Items.Add(aDictionary(1))
+        lstCollections.Items.Add(aDictionary("Key_1"))
+
+    End Sub
+End Class
